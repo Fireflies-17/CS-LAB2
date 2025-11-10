f72f4622701b450e252803b0da8f060806ae3a2d
3024244501 Zhu-Bingzheng <2401000536@qq.com>
2025-11-11 00:42:52 +0800

    biemenghan Linux Thinkbook16 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:42:52 up 30 min,  1 user,  load average: 0.00, 0.00, 0.00 53e25639751c173f918fe5760239a65bd2b0b4b

--- Shell/tsh.c
+++ Shell/tsh.c
@@ -284,7 +284,22 @@ int parseline(const char *cmdline, char **argv)
  */
 int builtin_cmd(char **argv) 
 {
-    return 0;     /* not a builtin command */
+	if (!strcmp(argv[0], "quit")) {
+		exit(0);
+	}
+	else if (!strcmp("&", argv[0])){
+		return 1;
+	}
+	else if (!strcmp("jobs", argv[0])) {
+		listjobs(jobs);
+		return 1;
+	}
+	else if (!strcmp("bg", argv[0]) || !(strcmp("fg", argv[0]))) {
+		//call bgfg
+		do_bgfg(argv);
+		return 1;
+	}
+	return 0;
 }
 
 /* 
